7b60e312507c842299579cb8d507b99aa467655a
Xon <tazipper1@hotmail.com>
2015-08-29 10:16:33 +0800

    Initial commit

---
+++ upload/library/SV/NoticeTime/Listener.php
@@ -0,0 +1,9 @@
+<?php
+
+class SV_NoticeTime_Listener
+{
+    public static function notices_prepare(array &$noticeList, array &$noticeTokens, XenForo_Template_Abstract $template, array $containerData)
+    {
+
+    }
+}
